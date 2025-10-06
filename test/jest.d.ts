72c4bb60f3e0a21f833eb71064ca2e3caff7e03b
JukLee0ira <meebookMonkey@163.com>
2025-10-06 15:01:15 -0400

    fix config

--- test/jest.d.ts
+++
@@ -1,9 +0,0 @@
-// Jest type declarations
-/// <reference types="jest" />
-
-declare namespace jest {
-  interface Matchers<R> {
-    toBeEthereumAddress(): R;
-    toBeValidBlockNumber(): R;
-  }
-}
