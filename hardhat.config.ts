9e7ba802e98212112e78ad4d2a87f93d02b69653
JukLee0ira <meebookMonkey@163.com>
2025-10-06 15:01:26 -0400

    fix config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomicfoundation/hardhat-network-helpers";
+import "@nomicfoundation/hardhat-verify";
 import dotenv from "dotenv";
 
 dotenv.config();
